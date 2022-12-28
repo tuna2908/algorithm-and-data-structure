64395809504d9e8aa9e99c519e4ca9d74de0832a
ohohohjkl <anhtuck01@gmail.com>
2022-12-28 23:36:14 +0700

    add: graph implementation

---
+++ datastructure/graphs/index.js
@@ -0,0 +1,16 @@
+//Edge List
+const graph_edges = [[0, 2], [2, 3], [2, 1], [1, 3]];
+
+//Adjacent List
+const graph_list = [[2],    //Node 0
+[2, 3], //Node 0
+[0, 1, 3], //Node 0
+[1, 2] //Node 0
+];
+//Adjacent Matrix
+const graph_matrix = [
+    [0, 0, 1, 0],   //Node 0
+    [0, 0, 1, 1],   //Node 1
+    [1, 1, 0, 1],   //Node 2
+    [0, 1, 1, 0]    //Node 3
+]
